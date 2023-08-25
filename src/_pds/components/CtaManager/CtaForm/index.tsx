c40e721c832ec4ef3ed2d8557f519b27ff456d95
imnotpizza <boooong772@naver.com>
2023-08-25 15:21:20 +0900

    Feat: CtaForm 컴포넌트 UI 추가

---
+++ src/_pds/components/CtaManager/CtaForm/index.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import Input from '../../Input';
+import Stack from '../../Stack';
+import Text from '../../Text';
+import { Props } from './index.types';
+
+const CtaForm = ({ page, buttonName, url }: Props) => {
+  return (
+    <Stack as="form" direction="column" gap={16}>
+      {/* 버튼 이름 */}
+      <Text variant="md-semibold" color="zinc_600" as="label">
+        버튼 이름 {page}
+        <Input
+          paddingY={16}
+          width="100%"
+          textVariant="md-semibold"
+          placeholder="버튼 이름 입력"
+        />
+        <Text variant="sm-semibold" color="cerise_500" marginTop={4}>
+          텍스트
+        </Text>
+      </Text>
+      {/* 버튼에 연결될 링크 */}
+      <Text variant="md-semibold" color="zinc_600" as="label">
+        링크
+        <Input paddingY={16} width="100%" textVariant="md-semibold" placeholder="링크 입력" />
+        <Text variant="sm-semibold" color="cerise_500" marginTop={4}>
+          텍스트
+        </Text>
+      </Text>
+      <SubmitButton type="submit">변경사항 저장하기</SubmitButton>
+    </Stack>
+  );
+};
+
+const SubmitButton = styled.button`
+  border-color: ${({ theme }) => theme.colors.black};
+  border-radius: 999px;
+  background-color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.white};
+
+  padding: 8px 12px;
+
+  &:hover {
+    border-color: ${({ theme }) => theme.colors.zinc_700};
+    background-color: ${({ theme }) => theme.colors.zinc_700};
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  &:disabled {
+    border-color: ${({ theme }) => theme.colors.zinc_200};
+    background-color: ${({ theme }) => theme.colors.zinc_200};
+    color: ${({ theme }) => theme.colors.zinc_400};
+    cursor: not-allowed;
+  }
+
+  transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
+    color 0.15s ease-in-out;
+`;
+
+export default CtaForm;
