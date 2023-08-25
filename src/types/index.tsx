04669f622412cb0e08cca231a1d26129e174f86f
imnotpizza <boooong772@naver.com>
2023-08-25 13:02:29 +0900

    Feat: Cta item 인터페이스 추가

---
+++ src/types/index.tsx
@@ -0,0 +1,27 @@
+export interface TCtaLinkInfo {
+  /**
+   * 페이지의 식별 아이디입니다.
+   *
+   * @default 0
+   */
+  page: number;
+  /**
+   * Cta 링크 관련 정보입니다.
+   *
+   * @default null
+   */
+  detail: {
+    /**
+     * Cta 버튼의 이름입니다.
+     *
+     * @default ''
+     */
+    buttonName: string;
+    /**
+     * Cta 버튼의 링크 url입니다.
+     *
+     * @default ''
+     */
+    url: string;
+  } | null;
+}
