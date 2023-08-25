09e4d1efb4c732bfeec62adeea2972bfd71317e0
imnotpizza <boooong772@naver.com>
2023-08-25 13:12:48 +0900

    Feat: CtaPageItem 컴포넌트 UI 추가

---
+++ src/_pds/components/CtaManager/CtaPageItem/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Flex from '../../Flex';
+import Stack from '../../Stack';
+import Text from '../../Text';
+import { CTAItemContainer } from './index.styles';
+import { Props } from './index.types';
+
+const CtaPageItem = ({ item, onClickItem, isSelected }: Props) => {
+  return (
+    <CTAItemContainer
+      padding={16}
+      alignItems="center"
+      justifyContent="space-around"
+      onClick={() => onClickItem(item)}
+      className={isSelected ? 'is-selected' : ''}
+    >
+      <Stack gap={4} direction="column">
+        <Text variant="md-semibold">{item.page} 페이지</Text>
+        <Flex alignItems="center">
+          <Text variant="sm-medium" color="zinc_400" marginRight={4}>
+            버튼
+          </Text>
+          <img src="/hand-tap.svg" alt="hand tap icon" width={14} />
+        </Flex>
+      </Stack>
+    </CTAItemContainer>
+  );
+};
+
+export default CtaPageItem;
